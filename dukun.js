d104c0016fcadf1f25c00038d896dbf62627bee3
BORUVW <30922681+BORUVW@users.noreply.github.com>
2017-08-13 17:09:36 +0700

    Update dukun.js

--- dukun.js
+++ dukun.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="/2017/08/10-tips-for-start-small-business-startup_12.html"}
 if(x=="3"){xxx="/2017/08/car-production-fall-hits-uk-industrial_12.html"}
 if(x=="4"){xxx="/2017/08/where-adsense-should-appear_12.html"}
 if(x=="5"){xxx="/2017/08/how-to-make-money-on-youtube_12.html"}
-if(x=="6"){xxx="/2017/08/how-to-make-money-with-bitcoin.html"}a_to_ve[i].href="https://mau.disingkat.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="/2017/08/how-to-make-money-with-bitcoin.html"}a_to_ve[i].href="http://mau.disingkat.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
